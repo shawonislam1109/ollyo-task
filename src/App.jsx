0a326c369672068a9fca87b96e0b2c20f07e0e3f
shawon <shawonislam1109@gmail.com>
2023-11-02 16:27:43 +0600

    something Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import route from "./route/route";
+import "./component/Home.css";
 
 function App() {
   return (
-    <div className="  mt-2 content-baseline w-9/12 shadow-xl  mx-auto">
+    <div className=" mt-5 content-baseline w-9/12 shadow-xl  mx-auto">
       <RouterProvider router={route}></RouterProvider>
     </div>
   );
--- src/route/route.jsx
+++ src/route/route.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../component/Home";
+import Example from "../component/Demo";
 
 const route = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "dnd",
+    element: <Example />,
   },
 ]);
 
